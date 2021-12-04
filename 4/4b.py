fc0c1990a3eef647b1133712ed919c41cafd46c6
Andy Nguyen <atnguyen@gmail.com>
2021-12-04 00:11:57 -0800

    Problem 4

---
+++ 4/4b.py
@@ -0,0 +1,84 @@
+import sys
+import numpy as np
+from collections import Counter
+
+class Board:
+	def __init__(self, board, id):
+		self.board = board
+		self.winner = np.zeros((5,5))
+		self.id = index
+		self.won = False
+
+	def mark_integer(self, val):
+		index = np.where(self.board == val)
+		self.winner[index[0], index[1]] = 1
+
+	def determine_win(self):
+		won = False
+		# Rows:
+		for r in range(5):
+			if np.sum(self.winner[r,:]) == 5:
+				won = True
+		# Columns
+		for c in range(5):
+			if np.sum(self.winner[:,c]) == 5:
+				won = True
+		# Diaganol
+		d_sum = np.sum([self.winner[x, x] for x in range(5)])
+		if d_sum == 5:
+			won = True
+
+		return won
+
+	def sum_unmarked(self):
+		result_array = np.where(self.winner == 0, self.board, 0)
+		result_array = result_array.astype(int)
+		s = np.sum(result_array)
+		return s
+
+	def get_id(self):
+		return self.id
+
+	def get_winner_str(self):
+		return str(self.winner)
+
+	def get_won(self):
+		return self.won
+
+	def set_won(self):
+		self.won = True
+
+	def get_board(self):
+		return self.board
+
+	def __repr__(self):
+		return str(self.board)
+
+# Input data
+
+with open('4.input.txt', 'r') as fh:
+	data = fh.read().split('\n\n')
+
+called_numbers = data[0].split(',')
+game_boards = list()
+
+# Process Boards
+for index, b in enumerate(data[1:], start=1):
+	board = b.split('\n')
+	board = [x.split() for x in board]
+	game_boards.append(Board(np.matrix(board), index))
+
+for c in called_numbers:
+	for g in game_boards:
+		g.mark_integer(c)
+		winning_number = g.determine_win()
+		if winning_number:
+			g.set_won()
+			result = [x.get_won() for x in game_boards]			
+			if Counter(result)[False] == 0:
+				print('Winning Number', c)
+				print(g.get_board())
+				s = g.sum_unmarked()
+				print('Unmarked Sum', s)
+				print('Final Value', s * int(c))
+				sys.exit()
