4ea3ccc8e137db3c0ab4a365874e5d395ced6b1c
Andy Nguyen <atnguyen@gmail.com>
2021-12-05 21:24:13 -0800

    Problem 5

---
+++ 5/5b.py
@@ -0,0 +1,86 @@
+import sys
+import numpy as np
+from itertools import chain
+
+entries = list()
+
+with open('5.input.txt', 'r') as fh:
+	entries = fh.read().split('\n')
+
+max_x = 0
+max_y = 0
+
+processed_entries = list()
+
+# Clean up data
+
+for e in entries:
+	elist = list(chain.from_iterable([i.split(',') for i in e.split(' -> ')]))
+	elist = [int(x) for x in elist]
+	processed_entries.append(elist)
+	x1,y1,x2,y2 = elist
+	# Find bounds for numpy array
+	if x1 > max_x:
+		max_x = x1
+	if x2 > max_x:
+		max_x = x2
+	if y1 > max_y:
+		max_y = y1
+	if y2 > max_y:
+		max_y = y2
+
+arr = np.zeros((int(max_x) + 1, int(max_y) + 1))
+
+for p in processed_entries:
+	x1,y1,x2,y2 = p
+
+	if x1 == x2:
+		# Horizontal
+		draw_length = abs(y1 - y2)
+		if y1 > y2:
+			ystart = y2
+		else:
+			ystart = y1
+		for y in range(draw_length + 1):
+			arr[ystart + y, x1] += 1
+	elif y1 == y2:
+		# Vertical
+		draw_length = abs(x1 - x2)
+		if x1 > x2:
+			xstart = x2
+		else:
+			xstart = x1
+		for x in range(draw_length + 1):
+			arr[y1, xstart + x] += 1
+	else:
+		# Diaganol
+		draw_length_x = abs(x1 - x2)
+		draw_length_y = abs(y1 - y2)
+		draw_length = abs(x1 - x2)   # Only 45 degree lines len(x) == len(y)
+		if x1 < x2:
+			xstart = x1
+			ystart = y1
+			if y1 < y2:
+				for x in range(draw_length + 1):
+					arr[ystart + x, xstart + x] += 1
+			else:
+				# y2 < y1
+				for x in range(draw_length + 1):
+					arr[ystart - x, xstart + x] += 1
+		else:
+			xstart = x2
+			ystart = y2
+			if y1 < y2:
+				for x in range(draw_length + 1):
+					arr[ystart - x, xstart + x] += 1
+			else:
+				# y2 < y1
+				for x in range(draw_length + 1):
+					arr[ystart + x, xstart + x] += 1
+
+
+print(arr)
+points = np.argwhere(arr > 1)
+print(points)
+print(len(points))
+ 
